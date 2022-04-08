4a16b8d319e2d26e07d02c0d3b30808cc73b31d1
CoderXGC <xgc@tom.com>
2022-04-08 13:05:40 +0800

    test2

--- src/main/java/com/ylesb/concurrent/test2/test2.java
+++ src/main/java/com/ylesb/concurrent/test2/test2.java
@@ -24,17 +24,37 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class test2 {
     //第一种创建线程的方式
+    //public static void main(String[] args) {
+    //    log.info("开始执行");
+    //    Thread t1 = new Thread(new Runnable() {
+    //        @Override
+    //        public void run() {
+    //            log.info("t1 run");
+    //        }
+    //    });
+    //    t1.setName("t1");
+    //    t1.start();
+    //    log.info("结束执行");
+    //}
+    //第二种创建线程的方式
     public static void main(String[] args) {
-        log.info("开始执行");
-        Thread t1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                log.info("t1 run");
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            log.debug("线程任务");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        });
-        t1.setName("t1");
+        }
+    };
+    // 可以使用lambda表达式呀
+    // Runnable runnable = ()->{ log.debug("这里完成线程的任务");};
+
+       Thread t1= new Thread(runnable,"t1");
         t1.start();
-        log.info("结束执行");
+        log.debug("主线程");
     }
 }
 
