57429b4dd8addd058fc6230ffb124747bab58937
CoderXGC <xgc@tom.com>
2022-04-09 16:49:14 +0800

    test2第三种创建线程方法

--- src/main/java/com/ylesb/concurrent/test2/test2.java
+++ src/main/java/com/ylesb/concurrent/test2/test2.java
@@ -10,6 +10,10 @@ package com.ylesb.concurrent.test2;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
 /**
  * @className    : test2
  * @description  : [描述说明该类的功能]  
@@ -37,24 +41,39 @@ public class test2 {
     //    log.info("结束执行");
     //}
     //第二种创建线程的方式
-    public static void main(String[] args) {
-    Runnable runnable = new Runnable() {
-        @Override
-        public void run() {
-            log.debug("线程任务");
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+    //public static void main(String[] args) {
+    //Runnable runnable = new Runnable() {
+    //    @Override
+    //    public void run() {
+    //        log.debug("线程任务");
+    //        try {
+    //            Thread.sleep(1000);
+    //        } catch (InterruptedException e) {
+    //            e.printStackTrace();
+    //        }
+    //    }
+    //};
+    //// 可以使用lambda表达式呀
+    //// Runnable runnable = ()->{ log.debug("这里完成线程的任务");};
+    //
+    //   Thread t1= new Thread(runnable,"t1");
+    //    t1.start();
+    //    log.debug("主线程");
+    //}
+    //第三种创建线程的方式
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        FutureTask futureTask = new FutureTask<>(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                log.debug("多线程任务");
+                Thread.sleep(5000);
+                return 100;
             }
-        }
-    };
-    // 可以使用lambda表达式呀
-    // Runnable runnable = ()->{ log.debug("这里完成线程的任务");};
-
-       Thread t1= new Thread(runnable,"t1");
-        t1.start();
+        });
+        // 主线程阻塞，同步等待 task 执行完毕的结果
+        new Thread(futureTask,"t1").start();
         log.debug("主线程");
+        log.debug("{}",futureTask.get());
     }
 }
 
