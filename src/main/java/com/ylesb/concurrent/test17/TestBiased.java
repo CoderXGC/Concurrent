d640479c1eeeab3efd3428b8a2c8cffddf56653c
CoderXGC <xgc@tom.com>
2022-05-07 18:46:27 +0800

    add lock

---
+++ src/main/java/com/ylesb/concurrent/test17/TestBiased.java
@@ -0,0 +1,38 @@
+package com.ylesb.concurrent.test17;
+/**
+ * @title: TestBiased
+ * @projectName Concurrent
+ * @description: TODO
+ * @author White
+ * @site : [www.ylesb.com]
+ * @date 2022/5/716:43
+ */
+
+import lombok.extern.slf4j.Slf4j;
+import org.openjdk.jol.info.ClassLayout;
+
+
+/**
+ * @className    : TestBiased
+ * @description  : [描述说明该类的功能]  
+ * @author       : [XuGuangchao]
+ * @site         : [www.ylesb.com]
+ * @version      : [v1.0]
+ * @createTime   : [2022/5/7 16:43]
+ * @updateUser   : [XuGuangchao]
+ * @updateTime   : [2022/5/7 16:43]
+ * @updateRemark : [描述说明本次修改内容] 
+ */
+@Slf4j
+public class TestBiased {
+    public static void main(String[] args) {
+    Dog dog = new Dog();
+
+        Object o = new Object();
+       System.out.println(ClassLayout.parseInstance(o).toPrintable());
+    log.info("1");
+    }
+}
+class Dog{
+
+}
